3741e8c606baae0e7f66d3b3519b15dd9c4981c4
David Shaub <dashaub@users.noreply.github.com>
2016-04-16 15:08:21 -0600

    Data now loads

--- Kohonen.java
+++ Kohonen.java
@@ -1,7 +1,18 @@
 //Kohonen.java
 /**
  * Visualize cell counts of a rectangular 
- * self-organizing map.
+ * self-organizing map. Thie program fits
+ * a Kohonen network to an input containing
+ * numeric data in a csv format.
+ * 
+ * 
+ * This file contains
+ * the main method and constructs the necessary
+ * windows for user input as well as reading in
+ * the data and validating it. It then passes
+ * the necesary data onto a SOM object where
+ * the model fitting is performed before
+ * displaying the results.
  * 
  * @author David Shaub
  * @version 1.0
@@ -15,9 +26,84 @@ import java.io.*;
 import java.util.*;
 public class Kohonen extends JFrame
 {
-	private JFrame window = new JFrame("Self-Organizing Map");
+	// Instance variables
 	private JButton fileChooser = new JButton("Input File");
-	private ArrayList <Double> inputData;
+	private ArrayList <Double[]> inputData = new ArrayList<Double[]>(); 
+	
+	/**
+	 * Read in the input csv data
+	 * This function reads the input data
+	 * from a csv for constructing the
+	 * self-organizing map. The function also
+	 * checks the data to make sure it is valid
+	 * and satisfies the necessary conditions
+	 * to construct a self-organizing map.
+	 * 
+	 * @param input The file object to read in
+	 * 
+	 * */
+	public void readFile(File input)
+	{
+		// Open a Scanner
+		Scanner inFile;
+		try
+		{
+			inFile = new Scanner(input);
+			inFile.useDelimiter("\n");
+		}
+		catch(IOException ioe)
+		{
+			JOptionPane.showMessageDialog(null, "Cannot read file. Select a readable file.");
+			return;
+		}
+		
+		// Treat the first line as a header and use it
+		// to determine the number of columns
+		int numColumns = 0;
+		String currentLine = inFile.next();
+		Scanner parser = new Scanner(currentLine);
+		parser.useDelimiter(",");
+		while(parser.hasNext())
+		{
+			parser.next();
+			numColumns++;
+		}
+		// There should be at least columns
+		if(numColumns < 2)
+		{
+			JOptionPane.showMessageDialog(null, "The file should have at least two columns.");
+			return;
+		}
+		// Now read the data
+		Double [] currentRow = new Double[numColumns];
+		while(inFile.hasNext())
+		{
+			currentLine = inFile.next();
+			parser = new Scanner(currentLine);
+			parser.useDelimiter(",");
+			try
+			{
+				// Ignore data in rows with more entries than in the header
+				for(int i = 0; i < numColumns; i++)
+				{
+					currentRow[i] = Double.parseDouble(parser.next());
+				}
+				inputData.add(currentRow);
+			}
+			// Ensure the data are parsed to numeric
+			catch(NumberFormatException nfe)
+			{
+				JOptionPane.showMessageDialog(null, "The file should contain only numeric data.");
+				return;
+			}
+			// Ensure a non-jagged array
+			catch(NoSuchElementException nsee)
+			{
+				JOptionPane.showMessageDialog(null, "Every row should contain one number for every columns in the file header.");
+				return;
+			}
+		}
+	}
 	
 	class ButtonListener implements ActionListener 
 	{
@@ -29,41 +115,28 @@ public class Kohonen extends JFrame
 		 * */
 		public void actionPerformed(ActionEvent e) 
 		{
-			System.out.println("Activated");
 			if(e.getSource() == fileChooser)
 			{
 				JFileChooser fc = new JFileChooser();
-				System.out.println("Running!");
 				fc.setCurrentDirectory(new File(System.getProperty("user.home")));
 				int result = fc.showOpenDialog(null);
 				if (result == fc.APPROVE_OPTION)
 				{
-					System.out.println(fc.getSelectedFile());
+					File inputFile = fc.getSelectedFile();
+					readFile(inputFile);
 				}
-				/**
-				try
-				{
-					Scanner inFile = new Scanner(new File("wines.csv"));
-				}
-				catch(IOException ioe)
-				{
-					JOptionPane.showMessageDialog(null, "Cannot read file. Select a valid file.");
-				}
-				* */
 			}
 		}
 	}
 	
 	public Kohonen()
 	{
-
-		
-		
 		//Construct the JFrame
-		
-		window.setSize (800, 200);
+		JFrame window = new JFrame("Self-Organizing Map");
+		window.setSize (650, 200);
 		window.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
 		window.setLayout(new FlowLayout());
+		window.setResizable(false);
 		
 		//Color
 		JLabel colorLabel = new JLabel("Shading");
@@ -76,6 +149,7 @@ public class Kohonen extends JFrame
 		JLabel epochLabel = new JLabel("Training epochs");
 		window.add(epochLabel);
 		JTextField epochs = new JTextField(4);
+		epochs.setText("500");
 		window.add(epochs);
 		
 		//Grid size
@@ -83,21 +157,22 @@ public class Kohonen extends JFrame
 		window.add(dimensionLabel);
 		JPanel gridElements = new JPanel();
 		JTextField xDim = new JTextField(3);
+		xDim.setText("5");
 		JTextField yDim = new JTextField(3);
+		yDim.setText("5");
 		window.add(xDim);
 		window.add(yDim);
 		
 		//Input
-		//JButton inputButton= new JButton("Input file");
 		ButtonListener bl = new ButtonListener();
 		fileChooser.addActionListener(bl);
 		window.add(fileChooser);
-		//JFileChooser fc = new JFileChooser();
-		//window.add(fc, BorderLayout.NORTH);
 		
 		window.setVisible (true);
 	}
 	
+	
+	// Main method
 	public static void main(String [] args)
 	{
 		Kohonen som = new Kohonen();
