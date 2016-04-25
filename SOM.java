dfa350eaccab9985bf8b1cd7a6f996937a36954d
David Shaub <dashaub@users.noreply.github.com>
2016-04-25 06:33:17 -0600

    training core

--- SOM.java
+++ SOM.java
@@ -44,6 +44,7 @@ public class SOM extends Grid
 		this.epochs = epochs;
 	}
 	
+	
 	/**
 	 * Train the SOM to the data.
 	 * This method runs the training
@@ -53,24 +54,128 @@ public class SOM extends Grid
 	 * */
 	public void train()
 	{
-		init();
+		this.init();
+		// Number of rows in the training data
 		int dataRows = this.gridData.length;
+		// Number of columns in the training data
 		int dataColumns = this.gridData[0].length;
-		int weightsRows;
+		// Number of rows (same number of columns) in the weights map
+		int weightsRows = this.weights[0].length;
+		// Number of rounds of training
 		int iterations = this.epochs * dataRows;
+		// Initial learning rate
+		double learningRate = 0.05;
+		// Initial neighborhood size
+		double neighborhood;
+		
+		// Current row being processed
 		int currentObs;
-		int nearestNode;
+		//int nearestNode;
+		// Nearest node to the current point
+		int nearest = 0;
+		// Smallest identified distance to a node
 		double nearestDistance;
 		
-		// Adapted from the C code for VR_onlineSOM in the R 'class' package
+		// Temporary variables for the current distance to a node
+		double dist;
+		double tmp;
+		
+		// "Unpack" the gridData, weights, and pair distances into a 1D array
+		double [] data = new double[dataRows * dataColumns];
+		double [] nodes = new double[weightsRows * weightsRows];
+		double [] distPairs = new double[pairArray.length * pairArray[0].length];
+		int count = 0;
+		for(int i = 0; i < dataColumns; i++)
+		{
+			for(int j = 0; j < dataRows; j++)
+			{
+				data[count] = this.gridData[i][j];
+				count++;
+			}
+		}
+		count = 0;
+		for(int i = 0; i < weightsRows; i++)
+		{
+			for(int j = 0; j < weightsRows; j++)
+			{
+				nodes[count] = this.weights[i][j];
+				count++;
+			}
+		}
+		count = 0;
+		for(int i = 0; i < this.pairArray.length; i++)
+		{
+			for(int j = 0; j < this.pairArray.length; j++)
+			{
+				distPairs[count] = this.pairArray[i][j];
+				count++;
+			}
+		}
+		
+		// Set the neighborhood to capture approximately 2/3 of the nodes.
+		// This is approximately 1.75 * variance (See Chebychev's inequality)
+		// https://en.wikipedia.org/wiki/Chebyshev's_inequality
+		neighborhood = 1.75 * variance(distPairs);
+		
+		// Adapted from the C code for VR_onlineSOM in the R "class" package
 		for(int i = 0; i < iterations; i++)
 		{
-			// Choose a random set of observations
+			// Choose a random observation for fitting
 			currentObs = (int)(Math.random() * dataRows);
 			// Find its nearest node
-			// Start with the first observation matching
-			// And the maximum distance possible
-			nearestNode = 0; nearestDistance = 1.7976931348623157E308;
+			// Start with the maximum distance possible
+			//nearestNode = 0;
+			//nearestDistance = 1.7976931348623157E308;
+			nearestDistance = Double.MAX_VALUE;
+			for(int j = 0; j < dataRows; j++)
+			{
+				// Reset the distance to zero for the next training point
+				dist = 0;
+				for(int k = 0; k < dataColumns; k++)
+				{
+					// For the current random observation and the current column,
+					// find the difference, i.e. the rectilinear distance
+					tmp = data[currentObs + k * dataRows] - nodes[j + k * weightsRows];
+					// dist^2 is the square of the sums of
+					// all the individual components, and
+					// minimizing distance^2 leads to the same
+					// node as minimizing distance.
+					dist += tmp * tmp;
+				}
+				// New closest node found
+				if(dist < nearestDistance)
+				{
+					//nearestNode = 0;
+					// Update the nearest node and distance
+					nearest = j;
+					nearestDistance = dist;
+				}
+			}
+			
+			// Update learning rate and neighborhood distances
+			// Initially "pull" the map by large amounts and
+			// pull nodes that are farther away (but within the
+			// neighborhood as well); as training
+			// continues create smaller distortions and apply
+			// them within a smaller neighborhood.
+			learningRate -= 0.04 * i / iterations;
+			neighborhood -= 1.0 * i / iterations;
+			// Prevent the neighborhood from becoming too small
+			neighborhood = (neighborhood < 0.5) ? 0.5 : neighborhood;
+			
+			// Apply the distortion to the map for nodes within
+			// the neighborhood
+			for(int l = 0; l < weightsRows; l++)
+			{
+				if(distPairs[l + weightsRows * nearest] <= neighborhood)
+				{
+					for(int m = 0; m < dataColumns; m++)
+					{
+						tmp = data[currentObs + m * dataRows] - nodes[l + m * weightsRows];
+						nodes[l + m * weightsRows] += tmp * learningRate;
+					}
+				}
+			}
 		}
 		
 		/**
@@ -98,17 +203,19 @@ public class SOM extends Grid
 	 * the training data.
 	 * 
 	 * */
-	public void init()
+	private void init()
 	{
 		// Prepare the output grid for 
 		// calculating pair distances
-		pairArray = new double[xDim][2];
-		for(int i = 0; i < xDim; i++);
+		pairArray = new double[xDim * yDim][2];
+		int count = 0;
+		for(int i = 0; i < xDim; i++)
 		{
 			for(int j = 0; j < yDim; j++)
 			{
-				pairArray[xDim][0] = xDim;
-				pairArray[xDim][1] = yDim;
+				pairArray[count][0] = i;
+				pairArray[count][1] = j;
+				count++;
 			}
 		}
 		
@@ -135,5 +242,5 @@ public class SOM extends Grid
 			}
 		}
 	}
-	Grid distGrid = new Grid(pairArray).distance();
+	//Grid distGrid = new Grid(pairArray).distance();
 }
